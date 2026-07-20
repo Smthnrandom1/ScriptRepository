77cd43e5d0890945d554675dfc6cf99f65b478b2
Smthnrandom1 <23ushmuh@gmail.com>
2026-07-20 17:19:23 +0200

    Update TeleportTool.lua

--- TeleportTool.lua
+++ TeleportTool.lua
@@ -1,1 +1,18 @@
-print("hello")
+-- a pretty simple teleport tool script
+
+local Players = game:GetService("Players")
+local lp = Players.LocalPlayer
+local lpMouse = lp:GetMouse()
+
+local teleportTool = Instance.new("Tool", lp.Backpack)
+teleportTool.Name = "TeleportTool"
+teleportTool.RequiresHandle = false
+
+teleportTool.Activated:Connect(function()
+	if not lp.Character then
+		return 
+	end
+	
+	local mousePosition = lpMouse.Hit.Position
+	lp.Character:MoveTo(mousePosition)
+end)
