4b2930ac3b7212fce08f681311b5eacafd3d0565
Smthnrandom1 <23ushmuh@gmail.com>
2026-07-20 17:12:33 +0200

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,5 +1,7 @@
 for i,v in pairs(workspace.Regen:GetChildren()) do
-	if v:IsA("Tool") then
+	if v:IsA("Model") and v:FindFirstChildOfClass("Tool") then
+		local v = v:FindFirstChildOfClass("Tool")
+		
 		if v:FindFirstChild("Handle") then
 			firetouchinterest(v.Handle, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
 			wait(0.1)
