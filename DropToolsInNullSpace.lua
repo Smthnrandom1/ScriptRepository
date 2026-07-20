cc0d3a013c9d4b323767b904292854a33d5c7cf9
Smthnrandom1 <23ushmuh@gmail.com>
2026-07-20 17:43:20 +0200

    Update DropToolsInNullSpace.lua

--- DropToolsInNullSpace.lua
+++ DropToolsInNullSpace.lua
@@ -2,6 +2,13 @@ local Players = game:GetService("Players")
 local lp = Players.LocalPlayer
 local backpack = lp.Backpack
 
+-- teleport the localplayer high up in the sky
+if lp.Character and lp.Character:FindFirstChild("HumanoidRootPart") then
+	lp.Character.HumanoidRootPart.CFrame = CFrame.new(0, math.huge, 0)
+elseif lp.Character and not lp.Character:FindFirstChild("HumanoidRootPart") then
+	lp.Character:PivotTo(CFrame.new(0, math.huge, 0))
+end
+
 -- equip every tool inside the backpack
 for i,v in pairs(backpack:GetChildren()) do
 	if v:IsA("Tool") then
@@ -9,6 +16,7 @@ for i,v in pairs(backpack:GetChildren()) do
 	end
 end
 
+-- drop every tool inside the character but if it cant be dropped put it back in the backpack
 for i,v in pairs(lp.Character:GetChildren()) do
 	if v:IsA("Tool") and v.CanBeDropped == true then
 		v.Parent = workspace
