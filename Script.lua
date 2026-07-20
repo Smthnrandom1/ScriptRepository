6423ebdd7491e9676965d2265b666f47c4514b29
Smthnrandom1 <23ushmuh@gmail.com>
2026-07-20 16:45:26 +0200

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,7 @@
-print("Hello World")
+local tool = Instance.new("Tool", game.Players.LocalPlayer.Backpack)
+tool.Name = "Github Tool"
+tool.RequiresHandle = false
+
+tool.Activated:Connect(function()
+	print("Github tool activated!")
+end)
